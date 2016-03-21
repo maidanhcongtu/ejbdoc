295b2a681453c08e6353f84c487cf77a5c0cc1c6
maidanhcongtu <maidanhcongtu@gmail.com>
2016-03-21 18:39:28 +0700

    initial project ejbdoc

---
+++ ejbdoc-ear/src/main/java/org/ejbdoc/ear/App.java
@@ -0,0 +1,13 @@
+package org.ejbdoc.ear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
